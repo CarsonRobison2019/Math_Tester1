6a3f6f5936d684773548eeabba92045a814494e1
Carson Robison <carson.robison.2019@student.lhschools.org>
2018-09-13 08:05:58 -0400

    moved from other folder

---
+++ src/mathtester.java
@@ -0,0 +1,34 @@
+public class mathtester {
+
+    public static void main(String[] args) {
+        String firstWord;
+        String SeconedWord;
+        int firstNumber,seconedNumber;
+
+        firstWord = "Dog";
+        SeconedWord = "house";
+        firstNumber = 5;
+        seconedNumber = 10;
+
+
+        System.out.println(firstWord+SeconedWord);
+        System.out.println(firstWord+seconedNumber);
+        System.out.println(+firstNumber+seconedNumber);
+        System.out.println(firstWord+(firstNumber+seconedNumber));
+        System.out.println("firstNumber + seconedNumber is "+ (firstNumber+seconedNumber));
+        System.out.println("firstNumber - seconedNumber is "+ (firstNumber-seconedNumber));
+        System.out.println("seconedNumber / firstNumber is "+ (seconedNumber/firstNumber));
+        System.out.println("firstNumber * seconedNumber is "+ (firstNumber*seconedNumber));
+        System.out.println("The modulus of the seconednumber by the firstnumber is "+ (seconedNumber%firstNumber));
+
+
+
+
+
+
+
+
+
+    }
+}
+
